c4bc8d84e3d75319390eb8789342ca5127f5f0e1
Nadeen <nadeenfindakly@gmail.com>
2025-01-18 18:20:18 -0500

    Adding login/create new user functionality

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, redirect, request, jsonify
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-bootstrap = Bootstrap(app)
+bootstrap=Bootstrap(app)
 item_links = []
-
+ 
+# Landing page
 @app.route('/')
-def index():
-    return render_template('app.html', links=item_links)
+# Wishlist page
+@app.route('/mycart')
+def my_shopping_cart():
+    return render_template('app.html', links=item_links)  
 
+# Adding links to wishlist page
 @app.route('/add-item', methods=['POST'])
 def add_item():
     global item_links
@@ -22,6 +26,31 @@ def add_item():
         return jsonify({"status": "success", "itemLink": item_link})
     else:
         return jsonify({"status": "error", "message": "No link provided"}), 400
+    
+
+# creating a database to store login credentials for users
+login_databasee = {'admin' : 'password'}
+
+# Creating a function to allow users to login from the login page
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    usr_name = request.form['username']
+    usr_pwd = request.form['password']
+    # existing user trying to login
+    if 'login' in request.form:
+        if usr_name in login_databasee and usr_pwd == login_databasee[usr_name]:
+            return render_template('app.html')
+        else:
+            return render_template('login.html')
+    # new user creating account and logging in
+    elif 'create_user' in request.form:
+        if usr_name not in login_databasee:
+            login_databasee[usr_name] = usr_pwd
+            return render_template('app.html')
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
