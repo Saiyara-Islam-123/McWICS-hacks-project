9229e90fe947d03d5a15e5fd523f4ceb7fc6f6c3
Saiyara <116515386+Saiyara-Islam-123@users.noreply.github.com>
2025-01-18 22:16:32 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import naive_bayes
-import csv
+
 import numpy as np
 import encode
 
@@ -17,6 +17,11 @@ def encode_date(datestring):
 
     return code
 
+def decode_date(c):
+    encoder = encode.Encoder()
+    date = encoder.code_to_date[c]
+    return date
+
 X_train = df["store id"].to_numpy().reshape(34, 1)
 y_train = df["sale_date"].apply(format).apply(encode_date).to_numpy().reshape(34, 1)
 
@@ -25,6 +30,7 @@ model = naive_bayes.NaiveBayes(X_train, y_train)
 def get_pred_and_prob(store, today_date):
     today = np.array([[encode_date(today_date)]])
 
-    return model.find_nearest_pred_sale_date_and_prob(np.array([[encode.encode_store(store)]]), today)
+    date, prob =  model.find_nearest_pred_sale_date_and_prob(np.array([[encode.encode_store(store)]]), today)
+    return decode_date(date), prob
 
-print(get_pred_and_prob("Zara", "4/5"))
+print(get_pred_and_prob("Zara", "7/9"))
